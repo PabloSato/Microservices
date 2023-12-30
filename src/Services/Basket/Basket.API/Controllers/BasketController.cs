b47614b05589594ce71910d89ce50ce422896271
Pablo Fernández Sato <pablofersato@gmail.com>
2023-12-30 13:40:31 +0100

    inyectamos el DiscountGrpc en Basket proyect

--- src/Services/Basket/Basket.API/Controllers/BasketController.cs
+++ src/Services/Basket/Basket.API/Controllers/BasketController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Net;
 using Basket.API.Entities;
+using Basket.API.GrpcServices;
 using Basket.API.Repositories;
 using Microsoft.AspNetCore.Mvc;
 
@@ -10,10 +11,11 @@ namespace Basket.API.Controllers;
 public class BasketController: ControllerBase
 {
     private readonly IBasketRepository _repository;
-
-    public BasketController(IBasketRepository repository)
+    private readonly DiscountGrpcService _discountGrpcService;
+    public BasketController(IBasketRepository repository, DiscountGrpcService discountGrpcService)
     {
         _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+        _discountGrpcService = discountGrpcService ?? throw new ArgumentNullException(nameof(discountGrpcService));
     }
 
     [HttpGet("{userName}", Name = "GetBasket")]
@@ -29,6 +31,22 @@ public class BasketController: ControllerBase
     [ProducesResponseType(typeof(ShoppingCart), (int)HttpStatusCode.OK)]
     public async Task<ActionResult<ShoppingCart>> UpdateBasket([FromBody] ShoppingCart basket)
     {
+        // Vamos a llamar al Discount.Grpc para obtener descuentos. OJITO!
+        // TODO: Communicate with Discount.Grpc and Calculate latest prices of product into shopping cart
+        // 1 - Communicate
+        //  1.1 Necesitamos instalar:
+        //   - dotnet tool install -g dotnet-grpc
+        //   - dotnet-grpc add-file path_to_file (que es el proto del otro proyecto)
+        
+        // 2 - Calculate latest prices
+        //  2.1 Tenemos que crear una clase que se encargue de consumir el Grpc para el decoplamiento
+        //  2.2 Obtenemos los descuentos de CADA item en el carrito
+        foreach (var item in basket.Items)
+        {   
+            var coupon = await _discountGrpcService.GetDiscount(item.ProductName);
+            // Recalculamos el precio
+            item.Price -= coupon.Amount;
+        }
         return Ok(await _repository.UpdateBasket(basket));
     }
 
