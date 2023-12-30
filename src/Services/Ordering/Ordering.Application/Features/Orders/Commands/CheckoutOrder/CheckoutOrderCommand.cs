e949d3a76430013a94039f993d14deeb60ce3eef
Pablo Fernández Sato <pablofersato@gmail.com>
2023-12-30 18:56:06 +0100

    application command features - updateorder

--- src/Services/Ordering/Ordering.Application/Features/Orders/Commands/CheckoutOrder/CheckoutOrderCommand.cs
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Commands/CheckoutOrder/CheckoutOrderCommand.cs
@@ -4,7 +4,6 @@ namespace Ordering.Application.Features.Orders.Commands.CheckoutOrder;
 
 public class CheckoutOrderCommand : IRequest<int>
 {
-    
    public string UserName {get; set;} 
    public decimal TotalPrice {get; set;} 
    
