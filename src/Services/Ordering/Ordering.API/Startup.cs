32b9ade8caf1fa3505881481df405e0c2444ba91
Pablo Fernández Sato <pablofersato@gmail.com>
2023-12-31 14:27:48 +0100

    infraestruture layer clean architecture - service registration

--- src/Services/Ordering/Ordering.API/Startup.cs
+++ src/Services/Ordering/Ordering.API/Startup.cs
@@ -15,7 +15,8 @@ namespace Basket.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddApplicationServices();
+            //services.AddApplicationServices();
+            //services.AddInfraestrutureServices();
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
